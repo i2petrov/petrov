9913efa990e264f774c173acb5424c4f7d6b53cf
Administrator <i2.petrov@mail.ru>
2025-11-07 08:48:16 +0300

    1333223lllll884

--- t2.py
+++ t2.py
@@ -57,8 +57,6 @@ print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
 
 print("Hello, World!")
 print("Hello, World!")
