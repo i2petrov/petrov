c789ab9b02f0b7cf4721f29960d3de4323a49ff3
Administrator <i2.petrov@mail.ru>
2026-08-20 10:34:08 +0300

    12312312

--- t1.py
+++ t1.py
@@ -22,10 +22,7 @@ print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
 
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
+
 print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
--- t2.py
+++ t2.py
@@ -23,10 +23,7 @@ print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
 
-print("Hello, World!")
 
-print("Hello, World!")
-print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
